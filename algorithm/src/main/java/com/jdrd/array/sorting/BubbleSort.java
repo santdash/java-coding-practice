9e48fa227d82ffea3b46b6ac9ad4036b9f035b5c
SantoshDash <santosh.dash@microfocus.com>
2023-04-09 22:39:46 +0530

    BubbleSort

---
+++ algorithm/src/main/java/com/jdrd/array/sorting/BubbleSort.java
@@ -0,0 +1,39 @@
+package com.jdrd.array.sorting;
+
+import java.util.function.BiFunction;
+
+/**
+ * Bubble Sort is the simplest sorting algorithm that works by repeatedly swapping the adjacent elements if they are
+ * in the wrong order.
+ * This algorithm is not suitable for large data sets as its average and worst-case time complexity is quite high.
+ * However, it can be optimized for smaller data sets and if less sorting is required.
+ */
+public class BubbleSort {
+    public static void sortAscending(int[] input) {
+        sort(input, ( a, b ) -> a > b);
+    }
+
+    public static void sortDescending(int[] input) {
+        sort(input, ( a, b ) -> a < b);
+    }
+
+    private static void sort(int[] input, BiFunction<Integer, Integer, Boolean> function) {
+        int n = input.length;
+        while (n > 0) {
+            /* Every iteration would move the largest/smallest element to the end, so the n-- is for reducing number
+            * of inner loop iterations.  while(true) would also work, just inner loop would iteration more times */
+            n--;
+            boolean swapped = false;
+            for (int i = 0; i < n; i++) {
+                if (function.apply(input[i], input[i + 1])) {
+                    int temp = input[i];
+                    input[i] = input[i + 1];
+                    input[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped)
+                break;
+        }
+    }
+}
