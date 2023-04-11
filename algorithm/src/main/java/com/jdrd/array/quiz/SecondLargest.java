2ff6254109411a51202a6feddf3ee394124f6695
SantoshDash <santosh.dash@microfocus.com>
2023-04-11 20:14:33 +0530

    11-Apr-2023 Second-largest element of array.

---
+++ algorithm/src/main/java/com/jdrd/array/quiz/SecondLargest.java
@@ -0,0 +1,23 @@
+package com.jdrd.array.quiz;
+
+/** Find the second-largest element in the array
+ * Constraint, array can contain only positive numbers.
+ * return -1, if it doesn't have second-largest element.
+ * */
+public class SecondLargest {
+
+    public static int find(int[] input){
+        int i =0, second = -1, largest = -1;
+        while ( i < input.length){
+            if ( input[i] > largest){
+                second = largest;
+                largest = input[i];
+            }
+
+            if ( input[i] != largest && input[i] > second)
+                second = input[i];
+            i++;
+        }
+        return second;
+    }
+}
