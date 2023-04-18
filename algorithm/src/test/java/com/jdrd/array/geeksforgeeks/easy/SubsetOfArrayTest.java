19ae12a1e7b5d0279d4412979b3487fde71ab3fc
Santosh Dash <santdash@yahoo.com>
2023-04-18 15:28:06 +0530

    18-Apr-2023: SubSet of Array

---
+++ algorithm/src/test/java/com/jdrd/array/geeksforgeeks/easy/SubsetOfArrayTest.java
@@ -0,0 +1,24 @@
+package com.jdrd.array.geeksforgeeks.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SubsetOfArrayTest {
+
+    @Test
+    void isSubset() {
+        long[] a1 = {1, 2, 3, 4, 5, 1, 1, 1};
+        long[] a2 = {1, 2, 3, 1};
+
+        assertEquals("Yes", SubsetOfArray.isSubset(a1, a2, 8, 4));
+    }
+
+    @Test
+    void isSubset_1() {
+        long[] a1 = {1, 2, 3, 4, 5};
+        long[] a2 = {1, 2, 3, 1};
+
+        assertEquals("No", SubsetOfArray.isSubset(a1, a2, 5, 4));
+    }
+}
